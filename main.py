f4a35f56f1873ca1fd991a70ebae253145800c9e
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-05-27 15:58:36 -0500

    Refactored main.py/Imported Dependency:
        - Refactore main.py to have each URL that is getting scraped in its own file in /scraping
        - Imported the following two dependencies:
    	- pymongo
    	- importlib
        - For loop that loops throught the page_urls dictionary to run the scrape_page function in each file in /scraping

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-import requests
-from bs4 import BeautifulSoup
+import pymongo
+import importlib
 
-URL = "https://stardewvalleywiki.com/Stardew_Valley_Wiki"
-r = requests.get(URL)
+# Connect to MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["stardew_info"]
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(r.content, 'html.parser')
+def main():
+    # URLs for the different pages on the wiki
+    page_urls = {
+        "home": "https://stardewvalleywiki.com/Stardew_Valley_Wiki",
+        "crops": "https://stardewvalleywiki.com/Crops",
+        "farm_animals": "https://stardewvalleywiki.com/Animals",
+        "fruit_trees": "https://stardewvalleywiki.com/Fruit_Trees",
+        "monsters": "https://stardewvalleywiki.com/Monsters",
+        "villagers": "https://stardewvalleywiki.com/Villagers",
+        "achievements": "https://stardewvalleywiki.com/Achievements",
+    }
 
-# Find and print the title of the webpage
-title = soup.title.string
-print(f"Title of the webpage: {title}")
+    # Dynamically import the scrape_page function from each module in the scraping directory
+    for page in page_urls:
+        module_name = f"scraping.{page}"
+        module = importlib.import_module(module_name)
+        scrape_page_func = getattr(module, "scrape_page")
+        
+        # Scrape data from the page and store it in the database
+        scraped_data = scrape_page_func(page_urls[page])
+        print(scraped_data)
+
+        # if scraped_data:
+            # Insert scraped data into MongoDB collection
+            # collection = db[page]
+            # collection.insert_one(scraped_data)
+
+if __name__ == "__main__":
+    main()
---
+++ scraping/achievements.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Function to scrape data from home page
+def scrape_page(url):
+    # Send a GET request to the website
+    r = requests.get(url)
+
+    # Parse the HTML content using BeautifulSoup
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        title = soup.title.string
+        return {"url": url, "title": title}
+    else:
+        print(f"Failed to retrieve the webpage: {url}")
+        return None
