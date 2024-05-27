b815bf735234065a6963e3004ebe66de1c7f2e85
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-05-27 13:36:23 -0500

    Added Dependency/Incorporated BeautifulSoup in main.py:
        - Added html5lib to requirements.txt
        - BeautifulSoup is now successfully pulling the title of the webpage and printing it

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import requests
-from bs4 import BeautfifulSoup
+from bs4 import BeautifulSoup
+
+URL = "https://stardewvalleywiki.com/Stardew_Valley_Wiki"
+r = requests.get(URL)
+
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(r.content, 'html.parser')
+
+# Find and print the title of the webpage
+title = soup.title.string
+print(f"Title of the webpage: {title}")
