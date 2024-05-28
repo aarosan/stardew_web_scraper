fde8e3802e4a183045c388740058533428fc36da
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-05-27 21:09:35 -0500

    farm_animals.py parsing code
        - Completed code to get all of the farm animal names from Stardew Valley Wiki

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def main():
     page_urls = {
         "home": "https://stardewvalleywiki.com/Stardew_Valley_Wiki",
         # "crops": "https://stardewvalleywiki.com/Crops",
-        # "farm_animals": "https://stardewvalleywiki.com/Animals",
-        "fruit_trees": "https://stardewvalleywiki.com/Fruit_Trees",
+        "farm_animals": "https://stardewvalleywiki.com/Animals",
+        # "fruit_trees": "https://stardewvalleywiki.com/Fruit_Trees",
         # "monsters": "https://stardewvalleywiki.com/Monsters",
         # "villagers": "https://stardewvalleywiki.com/Villagers",
         # "achievements": "https://stardewvalleywiki.com/Achievements",
--- scraping/farm_animals.py
+++ scraping/farm_animals.py
@@ -6,11 +6,23 @@ def scrape_page(url):
     # Send a GET request to the website
     r = requests.get(url)
 
-    # Parse the HTML content using BeautifulSoup
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.content, 'html.parser')
-        title = soup.title.string
-        return {"url": url, "title": title}
-    else:
-        print(f"Failed to retrieve the webpage: {url}")
-        return None
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+    farm_animals = []
+
+    # Find all <span> elements with class 'toctext'
+    spans = soup.find_all('span', class_='toctext')
+
+    # Extract the text from each <span> and add it to the list if it contains the word 'tree'
+    for span in spans[24:]:
+        text = span.get_text(strip=True)
+        if not any(word in text.lower() for word in ['animals', 'hutch', 'bugs', 'history', 'reference']):
+            farm_animals.append(text)
+
+        # if 'tree' in text.lower():
+            # fruit_trees.append(text)
+
+    for farm_animal in farm_animals:
+        print(farm_animal)
+
+    return farm_animals
