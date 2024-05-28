c096c7486f3fd13efdaad44d186cfcc6d31caa79
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-05-28 10:59:46 -0500

    monsters.py parsing code
        - Completed code to get all of the monster names from Stardew Valley Wiki

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def main():
         # "crops": "https://stardewvalleywiki.com/Crops",
         # "farm_animals": "https://stardewvalleywiki.com/Animals",
         # "fruit_trees": "https://stardewvalleywiki.com/Fruit_Trees",
-        # "monsters": "https://stardewvalleywiki.com/Monsters",
-        "villagers": "https://stardewvalleywiki.com/Villagers",
+        "monsters": "https://stardewvalleywiki.com/Monsters",
+        # "villagers": "https://stardewvalleywiki.com/Villagers",
         # "achievements": "https://stardewvalleywiki.com/Achievements",
     }
 
--- scraping/monsters.py
+++ scraping/monsters.py
@@ -7,10 +7,34 @@ def scrape_page(url):
     r = requests.get(url)
 
     # Parse the HTML content using BeautifulSoup
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.content, 'html.parser')
-        title = soup.title.string
-        return {"url": url, "title": title}
-    else:
-        print(f"Failed to retrieve the webpage: {url}")
-        return None
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    monsters = []  
+
+    a_tags = soup.find_all('a')
+
+    # Debug: Print the total number of <a> tags found
+    # print(f"Total <a> tags found: {len(a_tags)}")
+
+    # Debug: Print the <a> tags within the specified range
+    # for i, a_tag in enumerate(a_tags[170:248], start=170):
+    #     print(f"{i}: {a_tag}")
+
+    # Loop through each <a> element within the specified range
+    for a_tag in a_tags[170:248]:
+        # Get the 'title' attribute of the <a> element
+        monster = a_tag.get('title')
+        if monster:
+            monsters.append(monster)
+
+    # Print the villagers for debugging
+    for monster in monsters:
+        print(monster)
+
+    return monsters
+    
+
+# Example URL for testing
+url = "https://stardewvalleywiki.com/Monsters"
+scrape_page(url)
